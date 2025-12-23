0f508ad8f7475f85d997bc40c22623297834d54c
Chi Pham <chipv.bka@gmail.com>
2025-12-23 09:06:03 +0000

    remove unsued code

--- go-clean-architecture/database/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE books (
-    id    INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name  VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
-    year INT(4) NULL,
-    created_at timestamp NULL DEFAULT NULL,
-    updated_at timestamp NULL DEFAULT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
--- struct/club/club.go
+++
@@ -1,7 +0,0 @@
-package club
-
-type Club struct { // exported struct
-	Name  	string //exported field
-	Country string //exported field
-	couch 	string //unexported field
-}

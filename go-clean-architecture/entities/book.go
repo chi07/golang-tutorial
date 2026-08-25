beeae0eab62f9e4197d386ec02eadb3f0506007c
chi07 <chipv.bka@gmail.com>
2026-08-25 17:50:58 +0700

    update opensearch

--- go-clean-architecture/entities/book.go
+++
@@ -1,8 +0,0 @@
-package entities
-
-type Book struct {
-	Id 		uint	`gorm:"primary_key"`
-	Name 	string  `json:"name"`
-	Author 	string  `json:"author"`
-	Year	int	`json:"year"`
-}
--- struct/player/player.go
+++
@@ -1,7 +0,0 @@
-package player
-
-type Player struct {
-	Name, Position 	string //exported field
-	Number, Age	    uint //exported field
-	stadium string //unexported field
-}
